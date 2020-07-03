bfa39717abbf319ed134954b1d5e239a4a08d6e3
Mukesh A <amukesh.mk@gmail.com>
2020-07-03 16:02:14 +0100

    updated code to use annotated points from csv

--- topoints.py
+++ topoints.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import json
 
-df = pd.read_csv('export.csv')
+df = pd.read_csv('resources/export.csv')
 
 data = {}
 data['count'] = []
@@ -9,9 +9,9 @@ data['points'] = []
 for index, row in df.iterrows():
     data['count'].append(row['External ID'].replace('img-', '').replace('.jpg', ''))
     jsondump = json.loads(row['Label'])
-    data['points'].append(jsondump['objects'][0]['line'])
+    data['points'].append(json.dumps(jsondump['objects'][0]['line']))
 
 new_df = pd.DataFrame.from_dict(data)
 
 new_df.sort_values(by=['count'], inplace = True)
-new_df.to_csv('required-data.csv', index=False)
+new_df.to_csv('resources/required-data.csv', index=False)
--- video-overlay.py
+++ video-overlay.py
@@ -1,45 +1,36 @@
 import cv2
 import numpy as np
+import json
+import pandas as pd
 
 frameWidth = 640
 frameHeight = 480
 
-def getContours(img):
-    # contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    # contours, hierarchy = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-    
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        if area > 2000:
-            cv2.drawContours(imgCont, cnt, -1, (255, 0, 0), 3)
-
-
-kernel = np.ones((5, 5), np.uint8)
-
 cap = cv2.VideoCapture("resources/object_detect_test_vid.mp4")
+df = pd.read_csv('resources/required-data.csv')
+
+def draw_overlay(points):
+    prev_point = None
+    for point in points:
+        if prev_point == None:
+            prev_point = point
+        cv2.line(img, (int(prev_point['x']), int(prev_point['y'])), (int(point['x']), int(point['y'])), (255, 0, 0), 5)
+        prev_point = point
 
 count = 0
 while True:
     success, img = cap.read()
-    
     if not success:
         break
-    
+
     img = cv2.resize(img, (frameWidth, frameHeight))
-    imgCont = img.copy()
-    
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.GaussianBlur(img, (7, 7), 1)
-    img = cv2.Canny(img, 50, 50)
-    img = cv2.dilate(img, kernel, iterations=1)
-    img = cv2.erode(img, kernel, iterations=1)
-    
-    getContours(img)
 
-    cv2.imshow("Result", imgCont)
+    data = df.loc[df['count'] == count]
+    points = json.loads(data['points'].iloc[0])
+    draw_overlay(points)
+    
+    cv2.imshow("Result", img)
 
-    cv2.imwrite("output/img-" + str(count) + ".jpg", imgCont)
     
     if cv2.waitKey(1) == ord('q') or count > 40:
          break
