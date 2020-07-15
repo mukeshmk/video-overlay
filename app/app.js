5802720db893289c38ddf69af5be6fe1a16849b7
Mukesh A <amukesh.mk@gmail.com>
2020-07-15 09:46:15 +0100

    added overlay code for video

--- app/app.js
+++ app/app.js
@@ -40,12 +40,37 @@ function onReady() {
         src = new cv.Mat(height, width, cv.CV_8UC4);
         dst = new cv.Mat(height, width, cv.CV_8UC1);
         cap = new cv.VideoCapture(video);
+        console.log(data)
         setTimeout(processVideo, 0);
     }
 
     function stop() {
         console.log('paused or ended');
         streaming = false;
+    }
+
+    function drawFace(points) {
+        let j;
+        for(let i = 0; i < points.length; i++) {
+            j = i + 1
+            if(i + 1 == points.length) {
+                j = 0
+            }
+            cv.line(dst, new cv.Point(parseInt(points[i][0]), parseInt(points[i][1])),
+                new cv.Point(parseInt(points[j][0]), parseInt(points[j][1])), [0, 0, 255, 0], 2)
+        }
+    }
+
+    function overlayOnImage(frameData) {
+        if(frameData['points'] == '') {
+            return
+        }
+        points = JSON.parse(frameData['points']);
+        faces = JSON.parse(frameData['faces']);
+        
+        faces.forEach(face => {
+            drawFace([points[face[0]], points[face[1]], points[face[2]], points[face[3]]])
+        });
     }
 
     function processVideo() {
@@ -56,6 +81,16 @@ function onReady() {
         }
         const begin = Date.now();
         cap.read(src)
+
+        // To rotate the image
+        let dsize = new cv.Size(src.rows, src.cols);
+        let center = new cv.Point(src.cols / 2, src.rows / 2);
+        let M = cv.getRotationMatrix2D(center, 90, 1);
+        cv.warpAffine(src, dst, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
+        // to change colour
+        cv.cvtColor(dst, dst, cv.COLOR_RGBA2RGB);
+
+        overlayOnImage(data[count]);
 
         cv.imshow('canvasOutput', dst);
 
