aa29da2558b37fa8ea241decdc18f1e2882ef981
Mukesh A <amukesh.mk@gmail.com>
2020-09-24 09:56:24 +0100

    loaded images for annotation

--- 6d-annotations/6doverlay.py
+++ 6d-annotations/6doverlay.py
@@ -5,7 +5,8 @@ import pandas as pd
 from os import listdir
 from os.path import isfile, join
 
-PATH = '6d-annotations/resources/annotations'
+ANNOTATION_PATH = '6d-annotations/resources/annotations'
+IMAGE_PATH = '6d-annotations/resources/images'
 
 def read_annotations():
     cols = ['class', 'x0', 'y0', 'x1', 'y1', 'x2', 'y2',
@@ -14,10 +15,10 @@ def read_annotations():
 
     data = {}
 
-    annotationFiles = [f for f in listdir(PATH) if isfile(join(PATH, f))]
+    annotationFiles = [f for f in listdir(ANNOTATION_PATH) if isfile(join(ANNOTATION_PATH, f))]
 
     for annotationFile in annotationFiles:
-        with open(PATH + '/' + annotationFile, 'r') as f:
+        with open(ANNOTATION_PATH + '/' + annotationFile, 'r') as f:
             output = f.read().split()
             for i, o in enumerate(output):
                 if not cols[i] in data:
@@ -27,6 +28,19 @@ def read_annotations():
     df = pd.DataFrame.from_dict(data)
     return df
 
+def read_image():
+    imageFiles = [f for f in listdir(IMAGE_PATH) if isfile(join(IMAGE_PATH, f))]
+
+    for image in imageFiles:
+        img = cv2.imread(IMAGE_PATH + '/' + image)
+
+        cv2.imshow('image', img)
+
+        if cv2.waitKey(0) == ord('q'):
+             break
+
 if __name__ == "__main__":
     df = read_annotations()
+    read_image()
     print(df)
+    cv2.destroyAllWindows()
