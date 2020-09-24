5f8abfc1e4510d0e2057b5f8442d0948c655f062
Mukesh A <amukesh.mk@gmail.com>
2020-09-24 10:34:12 +0100

    added colours and random point scaling

--- 6d-annotations/6doverlay.py
+++ 6d-annotations/6doverlay.py
@@ -7,6 +7,24 @@ from os.path import isfile, join
 
 ANNOTATION_PATH = '6d-annotations/resources/annotations'
 IMAGE_PATH = '6d-annotations/resources/images'
+COLOURS = [
+    (0,0,0),
+ 	(255,255,255),
+ 	(255,0,0),
+ 	(0,255,0),
+ 	(0,0,255),
+ 	(255,255,0),
+ 	(0,255,255),
+ 	(255,0,255),
+ 	(192,192,192),
+ 	(128,128,128),
+ 	(128,0,0),
+ 	(128,128,0),
+ 	(0,128,0),
+ 	(128,0,128),
+ 	(0,128,128),
+ 	(0,0,128)
+]
 
 def read_annotations():
     cols = ['class', 'x0', 'y0', 'x1', 'y1', 'x2', 'y2',
@@ -23,17 +41,35 @@ def read_annotations():
             for i, o in enumerate(output):
                 if not cols[i] in data:
                     data[cols[i]] = []
-                data[cols[i]].append(float(o))
+                data[cols[i]].append(int(float(o)*1000))
     
     df = pd.DataFrame.from_dict(data)
     return df
 
-def read_image():
+def draw_overlay(img, points):
+    X = 'x'
+    Y = 'y'
+    LEN = 9
+
+    prev_point = None
+    for i in range(LEN):
+        if i == 0:
+            cv2.circle(img, (points.iloc[0][X+str(i)], points.iloc[0][Y+str(i)]), 3, (0, 255, 0), -1)
+        else:
+            if prev_point == None:
+                prev_point = [points.iloc[0][X+str(i)], points.iloc[0][Y+str(i)]]
+            cv2.line(img, (int(prev_point[0]), int(prev_point[1])), (points.iloc[0][X+str(i)], points.iloc[0][Y+str(i)]), COLOURS[i], 5)
+            prev_point = [points.iloc[0][X+str(i)], points.iloc[0][Y+str(i)]]
+
+
+    return img
+
+def read_image(df):
     imageFiles = [f for f in listdir(IMAGE_PATH) if isfile(join(IMAGE_PATH, f))]
 
-    for image in imageFiles:
+    for i, image in enumerate(imageFiles):
         img = cv2.imread(IMAGE_PATH + '/' + image)
-
+        img = draw_overlay(img, df.iloc[[i]])
         cv2.imshow('image', img)
 
         if cv2.waitKey(0) == ord('q'):
@@ -41,6 +77,5 @@ def read_image():
 
 if __name__ == "__main__":
     df = read_annotations()
-    read_image()
-    print(df)
+    read_image(df)
     cv2.destroyAllWindows()
